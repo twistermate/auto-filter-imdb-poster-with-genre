561ae00a39580b1337df99558e94a9f792cb0efb
twistermate <88696997+twistermate@users.noreply.github.com>
2021-10-01 07:07:54 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -25,19 +25,17 @@ Bot Owner Only
 /logger  Get Logs
 /delete Delete File From Database
 
-[➡️How To Add Old Files For database](https://youtu.be/mtu5hgW-l_o)
-
-@Mo_Tech_YT
+@SPACE_BOtZ
 """
 
 ABOUT = """
-➥ Name :  Auto Filter With IMDB
+➥ Name :  HHT
     
-➥ Creator : [Muhammed RK](t.me/Mrk_YT)
+➥ Creator : [Irfan](t.me/Makerwith)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Source Code : [Click Me](https://github.com/PR0FESS0R-99/LuciferMoringstar_Robot)
+➥ Source Code : [Click Me](https://t.me/SPACE_BOtZ)
 """
